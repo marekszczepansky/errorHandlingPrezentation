8ac6e0f944230f36b42218f3bcfad27c38f2b43a
marekszczepansky <marek.szczepansky@gmail.com>
2019-01-21 23:15:27 +0100

    Initial commit

---
+++ errorHandling/src/main/java/org/lhasa/errorHandling/ErrorHandlingApplication.java
@@ -0,0 +1,14 @@
+package org.lhasa.errorHandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErrorHandlingApplication.class, args);
+	}
+
+}
+
---
+++ errorHandling/src/test/java/org/lhasa/errorHandling/ErrorHandlingApplicationTests.java
@@ -0,0 +1,17 @@
+package org.lhasa.errorHandling;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ErrorHandlingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
